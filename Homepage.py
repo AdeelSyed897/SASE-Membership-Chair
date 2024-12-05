e9ae025705c0181a93fb60feae855e9ee72be8b2
AdeelSyed897 <82331921+AdeelSyed897@users.noreply.github.com>
2024-12-04 23:10:33 -0600

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -6,6 +6,8 @@ homePage = st.Page(page="Homepage.py",default=True)
 Create = st.Page(page="Pages/Create.py")
 Merge = st.Page(page="Pages/Merge.py")
 Visualize = st.Page(page="Pages/Visualize.py")
+pg = st.navigation(pages=[homePage,Create,Merge,Visualize])
+pg.run()
 
 st.title("WPI SASE Membership Chair")
 st.write("Welcome to the Membership Chair website for WPI’s Society of Asian Scientists and Engineers (SASE). This website streamlines the attendance tracking and gives valuable data on different events and demographics.")
