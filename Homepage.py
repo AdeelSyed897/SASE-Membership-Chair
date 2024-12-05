1360eecdef6aaa4afe99fe9db9cd4c7b720211bb
AdeelSyed897 <82331921+AdeelSyed897@users.noreply.github.com>
2024-12-04 23:12:49 -0600

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -1,13 +1,5 @@
 import streamlit as st
 
-# Page Configuration
-st.set_page_config()
-homePage = st.Page(page="Homepage.py")
-Create = st.Page(page="Pages/Create.py")
-Merge = st.Page(page="Pages/Merge.py")
-Visualize = st.Page(page="Pages/Visualize.py")
-pg = st.navigation(pages=[homePage,Create,Merge,Visualize])
-pg.run()
 
 st.title("WPI SASE Membership Chair")
 st.write("Welcome to the Membership Chair website for WPI’s Society of Asian Scientists and Engineers (SASE). This website streamlines the attendance tracking and gives valuable data on different events and demographics.")
